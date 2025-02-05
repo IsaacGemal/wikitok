f6db595672558d54bdb7fdd8a0f3e40d5c968b41
Fauzan Abdillah <57308558+fauzanebd@users.noreply.github.com>
2025-02-05 09:27:52 +0000

    change indonesian to bahasa indonesia

--- frontend/src/languages.ts
+++ frontend/src/languages.ts
@@ -92,7 +92,7 @@ export const LANGUAGES = [
   },
   {
     id: "id",
-    name: "Indonesian",
+    name: "Bahasa Indonesia",
     flag: "https://hatscripts.github.io/circle-flags/flags/id.svg",
     api: "https://id.wikipedia.org/w/api.php?",
     article: "https://id.wikipedia.org/?curid=",
